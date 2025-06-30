c6b16723ebaad85e7c77aa3d880b6567a447f592
carol <carolinalealrequena@gmail.com>
2025-06-30 18:08:57 +0200

    first project half

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { AddNewItem } from "./AddNewItem";
+import { Column } from "./Column";
+import { AppContainer } from "./styles";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Column text="Todo: " />
+      <AddNewItem
+        toggleButtonText="+ Add another list"
+        onAdd={() => console.log("Item created")}
+      />
+    </AppContainer>
   );
-}
-
-export default App;
+};
